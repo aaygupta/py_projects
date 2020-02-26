156c43767d8cbb85351cf831559703d1b1a5fb4f
Ankit Gupta <guptaa.ankitt@gmail.com>
2020-02-26 17:49:39 +0100

    Create memes_xkcd.py

---
+++ memes_xkcd.py
@@ -0,0 +1,31 @@
+# pip install requests, bs4
+import os, requests, bs4
+
+url = 'https://xkcd.com'
+os.makedirs('xkcd_memes', exist_ok=True)
+while not url.endswith('#'):
+    print('Downloading Image %s....' % url)
+    res = requests.get(url)
+    res.raise_for_status()
+
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    comic_elem = soup.select('#comic img')
+
+    if comic_elem==[]:
+        print('Could not find comic image.')
+    else:
+        comic_url = 'https:' + comic_elem[0].get('src')
+        print('Downloading image %s...' % comic_url)
+        res = requests.get(comic_url)
+        res.raise_for_status()
+
+        image_file = open(os.path.join('xkcd_memes', os.path.basename(comic_url)), 'wb')
+        for chunk in res.iter_content(100000):
+            image_file.write(chunk)
+        image_file.close()
+
+    prev_link = soup.select('a[rel="prev"]')[0]
+    url = 'https://xkcd.com' + prev_link.get('href')
+
+
+print('Done!')
