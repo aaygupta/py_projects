7f9ddf225d9b9351e7d91e6d2b086f052ab11c4d
Ankit Gupta <guptaa.ankitt@gmail.com>
2020-02-26 19:57:42 +0100

    another commit

--- memes_xkcd.py
+++ memes_xkcd.py
@@ -4,7 +4,7 @@ import os, requests, bs4
 url = 'https://xkcd.com'
 os.makedirs('xkcd_memes', exist_ok=True)
 while not url.endswith('#'):
-    print('Downloading Image %s....' % url)
+    print('Downloading page %s....' % url)
     res = requests.get(url)
     res.raise_for_status()
 
