513900cff81237518bfb7d08957c3e8b37147f65
Ankit Gupta <aaygupta@outlook.com>
2020-08-25 11:01:43 +0200

    Update twitter_scraper.py

--- twitter_scraper.py
+++ twitter_scraper.py
@@ -1,14 +1,4 @@
-import scrapy, re, logging
+from twitter_scraper import get_tweets
 
-
-class TwitterSpider(scrapy.Spider):
-    name = 'twitterspider'
-    # start_urls = ['https://mobile.twitter.com/hashtag/covid19']
-
-    def parse(self, response):
-        tweets = response.xpath('//table[@class="tweet  "]/@href').getall()
-        logging.info(f'{len(tweets)} tweets found')    
-        for tweet_id in tweets:        
-            tweet_id = re.findall("\d+", tweet_id)[-1]        
-            tweet_url = 'https://twitter.com/anyuser/status/'+str(tweet_id)           
-            yield scrapy.Request(tweet_url, callback=self.parse_tweet)
+for tweet in get_tweets('covid19', pages=1):
+    print(tweet['text'])
